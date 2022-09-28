3b02b730797971ce6ae3ef72e7a7359a2aa4ecae
auberr <auberr@daum.net>
2022-09-28 21:33:39 +0900

    0927 morning test update

--- introduce/models.py
+++ introduce/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-# class AccessLog(models.Model):
-#     create_at = model.
+class AccessLog(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.create_at} / {self.location}"
--- introduce/views.py
+++ introduce/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from introduce.models import AccessLog
 
 # Create your views here.
 def introduce(request):
+    # case 1
+    access_log = AccessLog()
+    access_log.location = 'introduce'
+    access_log.save()
+
+    # case 2
+    '''
+    AcessLog.ojbects.create(
+        location="introduce"
+    )
+    '''
+
+    
     return render(request, 'introduce.html')
