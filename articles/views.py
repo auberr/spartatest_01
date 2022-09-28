5db3141abc5b89de9ebc3f4d439741a5ad7f5d35
auberr <auberr@daum.net>
2022-09-28 11:36:48 +0900

    dinner views update

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menus)
+    context = {
+        'pick' : pick,
+        'name': name,
+    }
+    return render(request, 'dinner.html', context)
